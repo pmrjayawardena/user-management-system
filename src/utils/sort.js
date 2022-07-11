c51742a76162e4fe88e127d5260138d3b0011dac
Prabodha <pmrjayawardena@gmail.com>
2022-07-11 10:58:47 +0530

    -sort quick fix

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -2,10 +2,10 @@ export const sort = (users, field, type) => {
 	let fieldName = field == null ? 'first_name' : field;
 	let usersArr = [...users];
 	let sorted = usersArr.sort((a, b) => {
-		if (a[fieldName] < b[fieldName]) {
+		if (a[fieldName] > b[fieldName]) {
 			return -1;
 		}
-		if (a[fieldName] > b[fieldName]) {
+		if (a[fieldName] < b[fieldName]) {
 			return 1;
 		}
 	});
